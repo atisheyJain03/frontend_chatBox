058fb729e0f586c8ba6240b55c033ed0f556eb7d
Atishey Jain <atisheyjain1232@gmail.com>
2020-09-09 21:08:56 +0530

    new

--- frontend_chatBox/src/axios.js
+++ frontend_chatBox/src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // in this we are saving base url of page for api
 export default axios.create({
-  // baseURL: "https://young-garden-40671.herokuapp.com",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://young-garden-40671.herokuapp.com",
+  // baseURL: "http://localhost:8000",
 });
