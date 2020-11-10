f5da402143fb420cad67a88ed199b7f7a6bd6b94
Atishey jain <atisheyjain1232@gmail.com>
2020-11-10 20:50:40 +0530

    Added logout functionality
    1) Added logout functionality
    2) added loader in lohin page

--- frontend_chatBox/src/Loader/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Loader.css";
-
-function Loader() {
-  // this is dot loader
-  return <div className="loader">loading...</div>;
-}
-
-export default Loader;
--- frontend_chatBox/src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-// in this we are saving base url of page for api
-export default axios.create({
-  baseURL: "https://young-garden-40671.herokuapp.com",
-  // baseURL: "http://localhost:8000",
-});
